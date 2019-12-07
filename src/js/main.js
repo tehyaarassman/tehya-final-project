83f680e9d5dc1cbf224c6cb2e905eb857b8daff0
tehyaarassman <tehyaarassman@gmail.com>
2019-12-06 20:56:26 -0600

    fix nav coloring

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,16 @@
-'use strict';
+// 'use strict';
 
-// populate copyright in footer with current year
-const copyrightYear = document.querySelector('.copyright-year');
-const now = new Date();
-copyrightYear.innerHTML = now.getFullYear();
+// // populate copyright in footer with current year
+// const copyrightYear = document.querySelector('.copyright-year');
+// const now = new Date();
+// copyrightYear.innerHTML = now.getFullYear();
+// $(function () {
+//   setNavigation();
+// });
+$(document).ready(function () {
+  var url = window.location;
+  $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+  $('ul.nav a').filter(function() {
+       return this.href == url;
+  }).parent().addClass('active');
+});
